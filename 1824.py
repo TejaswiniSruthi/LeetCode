7a1615e99732a98c4fd76ac9bd1335f40b83ff02
TejaswiniSruthi <82491774+TejaswiniSruthi@users.noreply.github.com>
2022-11-06 02:42:22 +0530

    Update 1824.py

--- 1824.py
+++ 1824.py
@@ -1,10 +1,44 @@
-obstacles = [0,1,2,3,0]
-mat=[]
-n=len(obstacles)
-for i in range(3):
-    arr=[0]*n
-    mat.append(arr)
-    for j,ele in enumerate(obstacles):
-        if i+1==ele:
-            mat[i][j]=1
-while(True):
+def obs(i,j,mat):
+    if i==0:
+        if mat[i+1][j]!=1:
+            i+=1
+            return i
+        if mat[i+2][j]!=1:
+            i+=2
+            return i
+    if i==1:
+        if mat[i+1][j]!=1:
+            i+=1
+            return i
+        if mat[i-1][j]!=1:
+            i-=1
+            return i
+    if i==2:
+        if mat[i-1][j]!=1:
+            i-=1
+            return i
+        if mat[i-2][j]!=1:
+            i-=2
+            return i
+
+class Solution:
+    def minSideJumps(self, obstacles: List[int]) -> int:
+        if (1 not in obstacles and 2 in obstacles and 3 in obstacles) or (3 not in obstacles and 2 in obstacles and 1 in obstacles):
+            return 1
+        n=len(obstacles)
+        mat=[]
+        for i in range(3):
+            arr=[0]*n
+            mat.append(arr)
+            for j,ele in enumerate(obstacles):
+                if i+1==ele:
+                    mat[i][j]=1
+        i=1
+        j=ju=0
+        while(j!=n-1):
+            if mat[i][j+1]==0:
+                j+=1
+            else:
+                i=obs(i,j,mat)
+                ju+=1
+        return ju
